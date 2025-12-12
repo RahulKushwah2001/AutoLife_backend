aac1952984fc0a9be1bec78af93db5ffbd7498f5
Rahul Kushwah <rahulkushwah7465@gmail.com>
2025-12-12 15:39:36 +0530

    mongo disconnect for now

--- app/db/mongo.py
+++ app/db/mongo.py
@@ -1,5 +1,17 @@
+# from motor.motor_asyncio import AsyncIOMotorClient
+# from app.core.config import settings
+
+# client = AsyncIOMotorClient(settings.MONGO_URL)
+# db = client[settings.DB_NAME]
+
+
 from motor.motor_asyncio import AsyncIOMotorClient
 from app.core.config import settings
 
-client = AsyncIOMotorClient(settings.MONGO_URL)
-db = client[settings.DB_NAME]
+if not settings.MONGO_URL:
+    print("⚠️ MongoDB disabled — no MONGO_URL found.")
+    client = None
+    db = None
+else:
+    client = AsyncIOMotorClient(settings.MONGO_URL)
+    db = client[settings.DB_NAME]
